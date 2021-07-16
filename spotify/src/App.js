c783e65b5fb10918d748401727595d37fd436a99
Ingrid Oncken <ingrid.oncken@gmail.com>
2021-07-16 15:33:39 +0100

    sidebar

---
+++ spotify/src/App.js
@@ -0,0 +1,14 @@
+import "bootstrap/dist/css/bootstrap.min.css"
+import "./App.css"
+import Sidebar from "./Components/Sidebar"
+
+function App() {
+  return (
+    <div className="App">
+      Home
+      <Sidebar />
+    </div>
+  )
+}
+
+export default App
